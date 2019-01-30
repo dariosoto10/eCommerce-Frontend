cdfa497bf8fd3c93bd73b9076ebfd81b84fb0acb
dario <sotodario3@gmaill.com>
2019-01-30 12:25:28 -0400

    Add UpdateItem Feature

--- components/Meta.js
+++ components/Meta.js
@@ -2,7 +2,7 @@ import Head from 'next/head';
 
 const Meta = () => (
   <Head>
-    <meta charset="utf-8" />
+    <meta charSet="utf-8" />
     <link rel="shorcut icon" href="/static/favicon.png" />
     <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
     <title>Sick Fits!</title>
