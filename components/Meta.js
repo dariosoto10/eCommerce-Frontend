793837e5ade12ee5268247f8c93fc381611bc589
dario <sotodario3@gmaill.com>
2019-01-29 20:40:10 -0400

    First Commit

---
+++ components/Meta.js
@@ -0,0 +1,12 @@
+import Head from 'next/head';
+
+const Meta = () => (
+  <Head>
+    <meta charset="utf-8" />
+    <link rel="shorcut icon" href="/static/favicon.png" />
+    <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+    <title>Sick Fits!</title>
+  </Head>
+)
+
+export default Meta;
